08775793378fac445a20fce102c40264e5684d00
lalitver10 <lalitver10@gmail.com>
2025-09-27 19:48:56 +0530

    added contributor.js file

--- contributors.js
+++ contributors.js
@@ -8,7 +8,8 @@ import usman_Pervez from "./contributors/usman_pervez.json";
 import shamsul from "./contributors/shamsul.json";
 import annshiv from  "./contributors/annshiv.json";
 import Gehendra from "./contributors/Gehendra.json";
-import madhusha from contributors/madhusha.json
+import Lalit from "./contributors/lalit.json";
+import madhusha from "./contributors/madhusha.json"
 const contributors = [
     dennis,
     kushboo,
@@ -21,6 +22,7 @@ const contributors = [
     annshiv,
     Gehendra,
     madhusha,
+    Lalit
 ];
 
 export default contributors;
