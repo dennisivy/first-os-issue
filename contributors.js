c7d5ce44797499858613d59a143d1b5726920b6d
YashMathurIIIT <25bcs201@iiitdwd.ac.in>
2026-01-12 03:07:20 +0530

    Filled the details on the contributors.json file

--- contributors.js
+++ contributors.js
@@ -8,7 +8,8 @@ import usman_Pervez from "./contributors/usman_pervez.json";
 import shamsul from "./contributors/shamsul.json";
 import annshiv from  "./contributors/annshiv.json";
 import Gehendra from "./contributors/Gehendra.json";
-import madhusha from contributors/madhusha.json
+import madhusha from contributors/madhusha.json;
+import yash from "contributers/yash.json";
 const contributors = [
     dennis,
     kushboo,
@@ -21,6 +22,7 @@ const contributors = [
     annshiv,
     Gehendra,
     madhusha,
+    yash,
 ];
 
 export default contributors;
